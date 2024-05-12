ff5733cceb4d0fe401f9ee308fb220e6556da2bf
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2024-05-12 14:46:41 +0000

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="django-mysql-replication",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     description="MySQL DB change events by reading replication logs",
     author="Waqqas Jabbar",
